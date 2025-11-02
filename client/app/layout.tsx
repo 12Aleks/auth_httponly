eb779f89d98ba9b65259649cb00a1401566ab0f9
12Aleks <tanais2010@yahoo.com>
2025-11-02 11:14:49 +0100

    The code has been update

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -23,9 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en"
-          data-lt-installed="true"
-    >
+    <html
+        lang="en"
+        data-lt-installed="true"
+        suppressHydrationWarning >
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
